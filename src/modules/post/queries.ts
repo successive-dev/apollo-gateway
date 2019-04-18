e321d0e28fd5f8a28f71b7ed8f863aa25c64b3ac
successive-dev <vishal.tewatia@successive.tech>
2019-04-18 12:04:48 +0530

    fixed: sorted posts from latest to old

--- src/modules/post/queries.ts
+++ src/modules/post/queries.ts
@@ -1,9 +1,25 @@
 import { postService } from '../../services';
 
+function sortDate(a: any, b: any) {
+  const dateA = new Date(a.createdAt);
+  const dateB = new Date(b.createdAt);
+
+  if (dateA > dateB) {
+    return -1;
+  }
+  if (dateA < dateB) {
+    return 1;
+  }
+  // a must be equal to b
+  return 0;
+}
+
 const Query = {
   getPosts: async () => {
     console.log('inside getPosts');
-    return postService.getPosts();
+    const posts = await postService.getPosts();
+    posts.sort(sortDate);
+    return posts;
   },
 };
 export default Query;
